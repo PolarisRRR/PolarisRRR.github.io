777ec52013e6802a7faf61d40df0a7a04cbca5e5
Your Name <you@example.com>
2020-10-26 18:02:50 +0800

    完成用户列表功能的开发

---
+++ src/components/Buttonadd.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="btn" class="btns">
+      <!-- <el-button class="btnstyle" @click="show(n)"  :key="n" v-for="n in nums" type="info">{{n}}</el-button> -->
+      <div class="btnstyle"  :key="n" v-for="n in nums" type="info"> <el-button class="btnstyle" @click="show(n)"   type="info">{{n}}</el-button></div>
+    </div>
+</template>
+<script >
+export default {
+  data () {
+    return {
+      nums: [
+        1, 2, 3, 4, 5
+      ]
+    }
+  },
+  methods: {
+    show (n) {
+      console.log(n)
+      // alert()
+    }
+  }
+}
+
+</script>
+<style scoped>
+ .btns {
+    margin: 0 auto;
+    width: 90%;
+    display: flex;
+    flex-flow: row wrap;
+}
+.btnstyle {
+  flex: 0 0 30%;
+  margin: 5px;
+  text-align: center;
+}
+.el-button {
+  width: 180px;
+  height: 30px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,24 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+// import Buttons from '../components/Buttonadd.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -17,10 +28,10 @@ const router = new VueRouter({
 
 // 挂在路由导航守卫
 router.beforeEach((to, from, next) => {
-  // to 将要访问的路径
-  // from 代表从那个路径跳转而来
-  // next 是一个函数 表示放行
-  //   next() 放行   next('/login')强制跳转
+// to 将要访问的路径
+// from 代表从那个路径跳转而来
+// next 是一个函数 表示放行
+//   next() 放行   next('/login')强制跳转
   if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
